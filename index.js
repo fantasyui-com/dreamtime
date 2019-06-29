a4c70b1721070da5423d5fb8c51fad7e982d1b3e
fantasyui-com <captain@fantasyui.com>
2019-06-28 20:18:02 -0400

    RC1

--- index.js
+++ index.js
@@ -1,53 +1,40 @@
 const kebabCase = require('kebab-case');
 const path = require('path');
 const fs = require('fs');
-
 module.exports = function(obj={}){
-
     const EventEmitter = require('events');
     class MyEmitter extends EventEmitter {};
     const emitter = new MyEmitter();
-
     // PREPARATION OF OBJ METADATA
     if(obj._dreamtime === undefined){
-
       // prepare metadata
       const meta = {emitter};
       const data = [];
       obj._dreamtime = { meta, data };
-
       // test function will get input for root
       obj.testRun = function(input={}){
-
         // check if module exists
         // execute it, passing information to the next.
         // run will be called once program structure is established.
-
         // Append the end event.
         data.push(['end',{source:'test'}]);
         emitter.on('end', ({input, setup}) => {
           console.info(`end got data: `, JSON.stringify({input, setup}))
         });
-
-
         // Create the chain
         for(let index = 0; index<obj._dreamtime.data.length-1; index++){
           let [name, setup] = data[index];
           emitter.on(name, function({input, setup}){
             console.info(`${name} got data: `, JSON.stringify({input, setup}));
-
-
-
-                    if( !fs.existsSync(path.resolve(`./code_modules/${kebabCase(name)}`)) ){
-                      fs.mkdirSync(path.resolve(`./code_modules/${kebabCase(name)}`))
-                      fs.copyFileSync(`${__dirname}/templates/standard/index.js`, `./code_modules/${kebabCase(name)}/index.js`);
-                      fs.copyFileSync(`${__dirname}/templates/standard/package.json`, `./code_modules/${kebabCase(name)}/package.json`);
-                      fs.copyFileSync(`${__dirname}/templates/standard/test.js`, `./code_modules/${kebabCase(name)}/test.js`);
-                      let p = JSON.parse(fs.readFileSync(`./code_modules/${kebabCase(name)}/package.json`).toString());
-                      p.name = kebabCase(name);
-                      fs.writeFileSync(`./code_modules/${kebabCase(name)}/package.json`, JSON.stringify(p,null,'  '))
-                    }
-
+            if( !fs.existsSync(path.resolve(`./code_modules/${kebabCase(name)}`)) ){
+              fs.mkdirSync(path.resolve(`./code_modules/${kebabCase(name)}`), {recursive:true})
+              fs.copyFileSync(`${__dirname}/templates/standard/index.js`, `./code_modules/${kebabCase(name)}/index.js`);
+              fs.copyFileSync(`${__dirname}/templates/standard/package.json`, `./code_modules/${kebabCase(name)}/package.json`);
+              fs.copyFileSync(`${__dirname}/templates/standard/test.js`, `./code_modules/${kebabCase(name)}/test.js`);
+              let p = JSON.parse(fs.readFileSync(`./code_modules/${kebabCase(name)}/package.json`).toString());
+              p.name = kebabCase(name);
+              fs.writeFileSync(`./code_modules/${kebabCase(name)}/package.json`, JSON.stringify(p,null,'  '))
+            }
             const program = require(path.resolve(`./code_modules/${kebabCase(name)}/index.js`));
             const req = input;
             const res = {};
@@ -59,20 +46,13 @@ module.exports = function(obj={}){
             program(req, res, next);
           });
         }
-
         // Create missing modules
-
-
         // BOOT
         // Configuration complete, sending standard input to the first listener
-
         let [name, setup] = data[0];
         emitter.emit(name, {input, setup});
-
-
       } // run
     }
-
     // RETURN STAGE
     // Proxy is used to just gather up the program structure information,
     // it keeps returning it self logging a sequence of function calls.
