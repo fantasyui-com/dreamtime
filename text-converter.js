9640903a52ed5c45c0da16fbd6ed3593c64d2033
fantasyui-com <captain@fantasyui.com>
2019-07-16 08:04:43 -0400

    Fully operational Intermediate Language text converter.

--- text-converter.js
+++ text-converter.js
@@ -30,7 +30,7 @@ const lineType = function(lineText){
   let responseString = null;
   const typeMatch = lineText.match(/\s*(?<type>.*?)\s*:/);
   if(typeMatch){
-    responseString = typeMatch.groups.type;
+    responseString = kebabCase(typeMatch.groups.type);
   }
   return responseString;
 }
@@ -41,83 +41,79 @@ const lineInterpreter = function(lineText){
   let data = null
 
   if(0){
-  }else if(type === 'Program'){
+  }else if(type === 'program'){
     const patternList = [
       /.*?:\s*(?<name>.+?)\s*-\s*(?<description>.*)$/, // with description
       /.*?:\s*(?<name>.+?)$/ // no description
     ]
     data = matchPatterns(patternList, lineText);
-  }else if(type === 'Author'){
+  }else if(type === 'author'){
     const patternList = [
       /.*?:\s*(?<author>.+?)$/ // no description
     ]
     data = matchPatterns(patternList, lineText);
-  }else if(type === 'Keywords'){
+  }else if(type === 'keywords'){
     const patternList = [
       /.*?:\s*(?<keywords>.+?)$/ // no description
     ]
     data = matchPatterns(patternList, lineText);
     data.keywords = data.keywords.split(/, | |,/).map(i=>i.trim());
 
-  }else if(type === 'Procedure'){
+  }else if(type === 'procedure'){
     const patternList = [
       /.*?:\s*(?<name>.+?)\s*-\s*(?<description>.*)$/, // with description
       /.*?:\s*(?<name>.+?)$/ // no description
     ]
     data = matchPatterns(patternList, lineText);
-  }else if(type === 'Task'){
+  }else if(type === 'task'){
     const patternList = [
       /.*?:\s*(?<name>.+?)\s*-\s*(?<description>.*)$/, // with description
       /.*?:\s*(?<name>.+?)$/ // no description
     ]
     data = matchPatterns(patternList, lineText);
-  }else if(type === 'Task Modules'){
+  }else if(type === 'task-modules'){
     const patternList = [
       /.*?:\s*(?<modules>.+?)$/ // no description
     ]
     data = matchPatterns(patternList, lineText);
     data.modules = data.modules.split(/, | |,/).map(i=>i.trim());
-  }else if(type === 'Action'){
+  }else if(type === 'action'){
     const patternList = [
       /.*?:\s*(?<name>.+?)\s*-\s*(?<description>.*)$/, // with description
       /.*?:\s*(?<name>.+?)$/ // no description
     ]
     data = matchPatterns(patternList, lineText);
-  }else if(type === 'Action Modules'){
+  }else if(type === 'action-modules'){
     const patternList = [
       /.*?:\s*(?<modules>.+?)$/ // no description
     ]
     data = matchPatterns(patternList, lineText);
     data.modules = data.modules.split(/, | |,/).map(i=>i.trim());
-  }else if(type === 'Task Modules'){
+  }else if(type === 'action-parameters'){
     const patternList = [
-      /.*?:\s*(?<modules>.+?)$/ // no description
+      /.*?:\s*(?<parameters>.+?)$/ // no description
     ]
     data = matchPatterns(patternList, lineText);
-    data.modules = data.modules.split(/, | |,/).map(i=>i.trim());
-  }else if(type === 'Action Parameters'){
+  }else if(type === 'task-parameters'){
     const patternList = [
       /.*?:\s*(?<parameters>.+?)$/ // no description
     ]
     data = matchPatterns(patternList, lineText);
 
-  }else if(type === 'Procedure Test'){
+  }else if(type === 'procedure-test'){
     const patternList = [
-      /.*?:\s*(?<name>.+?)\s*-\s*(?<description>.*)$/, // with description
-      /.*?:\s*(?<name>.+?)$/ // no description
+      /.*?:\s*(?<description>.+?)\s*-\s*(?<assertion>.*)$/, // with description
     ]
     data = matchPatterns(patternList, lineText);
 
-  }else if(type === 'Task Test'){
+  }else if(type === 'task-test'){
     const patternList = [
-      /.*?:\s*(?<name>.+?)\s*-\s*(?<description>.*)$/, // with description
-      /.*?:\s*(?<name>.+?)$/ // no description
+      /.*?:\s*(?<description>.+?)\s*-\s*(?<assertion>.*)$/, // with description
     ]
     data = matchPatterns(patternList, lineText);
-  }else if(type === 'Action Test'){
+  }else if(type === 'action-test'){
     const patternList = [
-      /.*?:\s*(?<name>.+?)\s*-\s*(?<description>.*)$/, // with description
-      /.*?:\s*(?<name>.+?)$/ // no description
+      /.*?:\s*(?<description>.+?)\s*-\s*(?<assertion>.*)$/, // with description
     ]
     data = matchPatterns(patternList, lineText);
 
@@ -154,9 +150,9 @@ module.exports = function({textPath,codePath}){
     data:[]
   }
 
-  let activeProcedure;
-  let activeTask;
-  let activeAction;
+  let activeProcedure = {meta:{}, data:[]};
+  let activeTask = {meta:{}, data:[]};
+  let activeAction = {meta:{}, data:[]};
 
   textDataList.forEach((input,index)=>{
 
@@ -167,6 +163,7 @@ module.exports = function({textPath,codePath}){
     console.log(lineData)
 
     if(0){
+
     } else if (lineData.type === 'program') {
       program.meta.name = lineData.name
       program.meta.description = lineData.description
@@ -174,22 +171,52 @@ module.exports = function({textPath,codePath}){
     } else if (lineData.type === 'author') {
       program.meta.author = lineData.author
 
+    } else if (lineData.type === 'keywords') {
+      program.meta.keywords = lineData.keywords
+
+
     } else if (lineData.type === 'procedure') {
-      activeProcedure = { meta: lineInterpreter(lineText), data: [] };
+      activeProcedure = { meta: lineInterpreter(lineText), test: [], data: [] };
       program.data.push(activeProcedure);
 
     } else if (lineData.type === 'task') {
-      activeTask = { meta: lineInterpreter(lineText), data: [] };
+      activeTask = { meta: lineInterpreter(lineText), test: [], data: [] };
       activeProcedure.data.push(activeTask);
 
     } else if (lineData.type === 'action') {
-      activeFunction = { meta: lineInterpreter(lineText), data: [] };
-      activeTask.data.push(activeFunction);
+      activeAction = { meta: lineInterpreter(lineText), test: [], data: [] };
+      activeTask.data.push(activeAction);
+
+
+    } else if (lineData.type === 'action-parameters') {
+      activeAction.meta.parameters = lineData.parameters;
+
+    } else if (lineData.type === 'task-parameters') {
+      activeTask.meta.parameters = lineData.parameters;
+
+    } else if (lineData.type === 'task-modules') {
+      activeTask.meta.modules = lineData.modules;
+
+
+    } else if (lineData.type === 'action-modules') {
+      activeAction.meta.modules = lineData.modules;
+
+    } else if (lineData.type === 'procedure-test') {
+      let {description, assertion} = lineData;
+      activeProcedure.test.push({description, assertion});
+
+    } else if (lineData.type === 'task-test') {
+      let {description, assertion} = lineData;
+      activeTask.test.push({description, assertion});
+
+     } else if (lineData.type === 'action-test') {
+      let {description, assertion} =  lineData;
+      activeAction.test.push({description, assertion});
+
+
+    } else {
+      console.log('Unsupported type', lineData.type);
     }
-
-
-
-
 
 
 
